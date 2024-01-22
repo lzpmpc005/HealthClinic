5d10e0c1468de229c7bdad26f169a8a85bac635c
vincentyelpen <147992850+vincentyelpen@users.noreply.github.com>
2024-01-22 17:43:30 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// main.cpp
 #include "crow.h"
 #include "crow/app.h"
 #include "crow/task_timer.h"
@@ -25,11 +26,13 @@
 #include "SqliteORM.hpp"
 
 #include "InventoryManager.hpp"
+#include "StockChecker.hpp"  // 新添加的头文件
 
 int main()
 {
     crow::SimpleApp app;
 
+    // 初始化数据库和仓库
     SqliteORM *orm = new SqliteORM();
     orm->connect("clinic.db");
     std::shared_ptr<PatientRepository> patients = std::make_shared<PatientRepository>(orm);
@@ -41,13 +44,27 @@ int main()
     std::shared_ptr<DoctorService> doctorService = std::make_shared<DoctorService>(doctors, patients, prescriptions);
     std::shared_ptr<AppointmentService> appointmentService = std::make_shared<AppointmentService>(appointments);
 
+    InventoryManager inventoryManager;
+    inventoryManager.addItem("vaccine", 50);
+
+    // 在新线程中启动库存检查
+    std::thread stockCheckerThread([&]() {
+        StockChecker stockChecker(inventoryManager, "vaccine", 10);
+        stockChecker.startChecking();
+    });
+    // 创建控制器和服务
     std::shared_ptr<PatientController> patientController = std::make_shared<PatientController>(app, patientService, appointmentService);
     std::shared_ptr<DoctorController> doctorController = std::make_shared<DoctorController>(app, doctorService);
     std::shared_ptr<InsuranceRepository> insuranceRepo = std::make_shared<InsuranceRepository>(orm);
     std::shared_ptr<InsuranceService> insuranceService = std::make_shared<InsuranceService>(insuranceRepo);
     std::shared_ptr<InsuranceController> insuranceController = std::make_shared<InsuranceController>(app, insuranceService);
 
+
+
+    // 启动应用
     app.port(18080).multithreaded().run();
+    stockCheckerThread.join();
+
 
     return 0;
 }
